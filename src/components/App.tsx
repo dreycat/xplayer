f7983fba10874c017296c042c5d53a376c63d07f
dreycat <dreycat@yandex.ru>
2020-04-09 21:57:20 +0500

    init

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Toggler } from './Toggler';
+
+function App() {
+  return (
+    <div className="App">
+      <Toggler />
+    </div>
+  );
+}
+
+export default App;
