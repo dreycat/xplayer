52589b0517b3bc88d9bb1cc1dfec4f9fd015b7ae
dreycat <dreycat@yandex.ru>
2020-04-11 17:36:24 +0500

    add playlist

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -2,42 +2,64 @@ import React, { useRef, useEffect } from 'react';
 import { useMachine } from '@xstate/react';
 import { Machine, assign } from 'xstate';
 
-const audioMachine = Machine({
+import playlist from './playlist';
+import { ITrack } from './types';
+
+interface IContext {
+  audioRef: HTMLAudioElement | null;
+  currentTrack: ITrack;
+}
+
+const getPlaylistTransitions = (target: string) => ({
+  PREV_TRACK: {
+    target,
+    actions: 'prevTrack',
+  },
+  NEXT_TRACK: {
+    target,
+    actions: 'nextTrack',
+  },
+  CHANGE_TRACK: {
+    target,
+    actions: 'changeTrack',
+  },
+});
+
+const audioMachine = Machine<IContext>({
   id: 'audio',
   initial: 'loading',
   context: {
     audioRef: null,
+    currentTrack: playlist[0],
   },
   states: {
     loading: {
       on: {
         LOADED: {
-          target: 'prepared',
+          target: 'paused',
           actions: 'setAudioRef',
         },
         FAIL: 'failure',
       },
     },
-    prepared: {
-      initial: 'paused',
-      states: {
-        paused: {
-          on: {
-            PLAY: {
-              target: 'playing',
-              actions: 'play',
-            },
-          },
+    paused: {
+      on: {
+        PLAY: {
+          target: 'playing',
+          actions: 'play',
         },
-        playing: {
-          on: {
-            PAUSE: {
-              target: 'paused',
-              actions: 'pause',
-            },
-            FAIL: '#audio.failure',
-          },
+        FAIL: 'failure',
+        ...getPlaylistTransitions('paused'),
+      },
+    },
+    playing: {
+      on: {
+        PAUSE: {
+          target: 'paused',
+          actions: 'pause',
         },
+        FAIL: 'failure',
+        ...getPlaylistTransitions('playing'),
       },
     },
     failure: {
@@ -46,6 +68,7 @@ const audioMachine = Machine({
           target: 'loading',
           actions: ['setAudioRef', 'load'],
         },
+        ...getPlaylistTransitions('loading'),
       },
     },
   },
@@ -55,24 +78,41 @@ const setAudioRef = assign({
   audioRef: (_, event: any) => event.audioRef,
 });
 
-const play = (context: any, _: any) => {
+const play = (context: IContext) => {
   context.audioRef?.play();
 };
 
-const pause = (context: any, _: any) => {
+const pause = (context: IContext) => {
   context.audioRef?.pause();
 };
 
-const load = (context: any, _: any) => {
+const load = (context: IContext) => {
   context.audioRef?.load();
 };
 
+const nextTrack = assign(({ currentTrack }: IContext) => {
+  const { id: trackId } = currentTrack;
+  const { length } = playlist;
+  const newTrack = trackId < length - 1 ? playlist[trackId + 1] : currentTrack;
+  return {
+    currentTrack: newTrack,
+  };
+});
+
+const prevTrack = assign(({ currentTrack }: IContext) => {
+  const { id: trackId } = currentTrack;
+  const newTrack = trackId > 0 ? playlist[trackId - 1] : currentTrack;
+  return {
+    currentTrack: newTrack,
+  };
+});
+
 const Player = () => {
-  const [state, send] = useMachine(audioMachine, { actions: { setAudioRef, play, pause, load } });
+  const [state, send] = useMachine(audioMachine, { actions: { setAudioRef, play, pause, load, nextTrack, prevTrack } });
   const audioRef = useRef<HTMLAudioElement>(null);
 
   console.log('value: ', state.value);
-  // console.log(state.context);
+  console.log(state.context);
 
   useEffect(() => {
     if (state.value !== 'failure') return;
@@ -87,20 +127,25 @@ const Player = () => {
   return (
     <>
       <audio
-        src="http://ice6.somafm.com/groovesalad-256-mp3"
+        src={state.context.currentTrack.url}
         ref={audioRef}
         onCanPlay={() => send('LOADED', { audioRef: audioRef.current })}
+        autoPlay={state.matches('playing')}
         onError={() => {
           console.log('error');
           send('FAIL');
         }}
       />
-      {state.matches({ prepared: 'playing' }) ? (
+      {state.matches('playing') ? (
         <button onClick={() => send('PAUSE')}>pause</button>
       ) : (
         <button onClick={() => send('PLAY')}>play</button>
       )}
-      {state.matches('failure') && <span>что-то пошло не так</span>}
+      <h2>{state.context.currentTrack.name}</h2>
+      <p>{state.context.currentTrack.title}</p>
+      <button onClick={() => send('PREV_TRACK')}>prev</button>
+      <button onClick={() => send('NEXT_TRACK')}>next</button>
+      {state.matches('failure') && <p>что-то пошло не так</p>}
     </>
   );
 };
---
+++ src/components/Player/types.ts
@@ -0,0 +1,7 @@
+export interface ITrack {
+  id: number;
+  name: string;
+  url: string;
+  title: string;
+  isRadio: boolean;
+}
